9a079a0e7241386289df3355e8285bd0e3a43974
Vger <vcdelariva@gmail.com>
2018-07-24 22:25:11 -0500

    Add display inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,8 +16,68 @@ var sqlConfig = {
 // create the connection information for the sql database
 var connection = mysql.createConnection(sqlConfig);
 
-
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    
+    purchaseItem();
+  });
   
 // ____________________________________________________________________________________
 // Functions
+// ____________________________________________________________________________________
+
+function purchaseItem() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+
+        displayItems(results);
+
+        inquirer.prompt([
+            {
+                name: "itemNumber",
+                type: "input",
+                message: "Which item would you like to purchase?",
+                validate: function(num){
+                    if (parseInt(num) < results.length) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "qty",
+                type: "input",
+                message: "How many would you like to purchase?",
+                // validate: function(num){
+                //     if (parseInt(num) < results.length) {
+                //         return true;
+                //     }
+                //     return false;
+                // }
+
+            }
+          ])
+          .then(function(answer) {
+            console.log(`Answer: ${answer.itemNumber}\nQty: ${answer.qty}`);
+          });
+
+    });
+    connection.end();
+}
+
+// ____________________________________________________________________________________
+
+function displayItems(list) {
+
+    console.log(chalk.blue("\nItem #  Product                   Department      Price($)       Available Qty"));
+    console.log(chalk.blue("------------------------------------------------------------------------------"));
+
+    // Display the inventory
+    for (var i =0; i < list.length; i++) {
+        console.log(`${[i].toString().padEnd(7)} ${list[i].product_name.padEnd(25)} ${list[i].department.padEnd(17)} ${list[i].customer_price.toString().padEnd(17)} ${list[i].stock_quantity}`);
+    }
+    console.log("\n");
+}
+
 // ____________________________________________________________________________________
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -8,39 +8,39 @@ CREATE TABLE products(
   product_name VARCHAR(100) NOT NULL,
   department VARCHAR(45) NOT NULL,
   customer_price DECIMAL(10,2) default 0,
-  stock_quanity INT default 0,
+  stock_quantity INT default 0,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Wilson Pro Staff", "Equipment", 219.00, 100);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Babolat Pure Aero", "Equipment", 209.00, 89);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Prince Text", "Equipment", 189.00, 70);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Adidas Skirt", "Apparel", 55.00, 43);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Nike Shorts", "Apparel", 75.00, 30);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Nike Tank", "Apparel", 50.00, 37);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Adidas Top", "Apparel", 55.00, 57);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
-VALUES ("Adidas Ubersonic Shoes", "Shoes", 124.95, 28);
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
+VALUES ("Adidas Ubersonic", "Shoes", 124.95, 28);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
-VALUES ("Nike Air Zoom Shoes", "Shoes", 140.00, 23);
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
+VALUES ("Nike Air Zoom", "Shoes", 140.00, 23);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
-VALUES ("Tennis Balls Case", "Equipment", 78.99, 125);
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
+VALUES ("Case Tennis Balls", "Equipment", 78.99, 125);
 
-INSERT INTO products (product_name, department, customer_price, stock_quanity)
+INSERT INTO products (product_name, department, customer_price, stock_quantity)
 VALUES ("Wilson Tennis Bag", "Equipment", 119.00, 15);
